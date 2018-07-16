092d3ee85aab6f0346c5a313a50874ea58cc708d
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-07-16 11:49:37 +0300

    Added compile, clean and tslint tasks to gulpfile. Added dev-dependencies as needed.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,9 +1,41 @@
 var gulp = require("gulp");
+var cache = require('gulp-cached');
+var tslint = require("gulp-tslint");
+
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+
+var sourceFilePath = "src/**/*";
+var distFolder = "dist";
 
 gulp.task("default", function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("compile", ["tslint"], function  (){
+  gulp.src(sourceFilePath + ".{js,json}")
+    .pipe(cache('copying'))
+    .pipe(gulp.dest(distFolder));
+
+  return tsProject.src()
+    .pipe(cache('compiling'))
+    .pipe(tsProject())
+    .js.pipe(gulp.dest(distFolder))
+
+});
+
+gulp.task("tslint", function () {
+  tsProject.src()
+    .pipe(cache('linting'))
+    .pipe(tslint({
+      configuration: "tslint.json"
+    }))
+    .pipe(tslint.report())
+});
+
+gulp.task("clean", function () {
+  return gulp.src(distFolder, { read: false })
+    .pipe(clean());
 });
