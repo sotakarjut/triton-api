98a393187af448f28e94424d25f626c08de34337
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-08-02 15:27:31 +0300

    Added connecting to mongo db to app.

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,11 +2,21 @@ import bluebird from "bluebird";
 import bodyParser from "body-parser";
 import express from "express";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 // Create Express server
 const app = express();
 
 // Connect to MongoDB
+const mongoUrl = MONGODB_URI;
+(mongoose as any).Promise = bluebird;
+mongoose.connect(mongoUrl, {useNewUrlParser: true }).then(
+  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch( (err) => {
+  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+  // process.exit();
+});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
