d804be2c64822f78a6b34db70622c3c0e98d5e52
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-08-08 09:56:34 +0300

    Added new model Role.

---
+++ app/src/models/Role.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt-nodejs";
+import mongoose from "mongoose";
+
+export type RoleModel = mongoose.Document & {
+  _id: string
+  canHack: boolean,
+  canImpresonate: boolean,
+  hackerLevel: number
+};
+
+const roleSchema = new mongoose.Schema({
+   _id: String,
+   canHack: Boolean,
+   canImpresonate: Boolean,
+   hackerLevel: Number
+
+}, );
+
+const roleModel = mongoose.model("User", roleSchema);
+export default roleModel;
