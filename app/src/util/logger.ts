4e07307cce4b23b71be07763c23265b662113a43
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-08-26 21:26:16 +0300

    Improved logging format and added timestamps to log.

--- app/src/util/logger.ts
+++ app/src/util/logger.ts
@@ -1,17 +1,31 @@
 import fs from "fs";
 import path from "path";
-import winston from "winston";
+import { createLogger, format, transports} from "winston";
 
 import { ENVIRONMENT } from "./secrets";
+
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
 const logDir = "log";
 if ( !fs.existsSync( logDir ) ) {
     fs.mkdirSync( logDir );
 }
 
-const logger = winston.createLogger({
+const logger = createLogger({
+    format: combine(
+    format.timestamp({
+            format: "YYYY-MM-DD HH:mm:ss"
+        }),
+    myFormat
+    ),
     transports: [
-        new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
-        new (winston.transports.File)({ filename: path.join(logDir, "/debug.log"), level: "debug"})
+        new (transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
+        new (transports.File)({ filename: path.join(logDir, "/debug.log"), level: "debug"}),
+        new (transports.File)({ filename: path.join(logDir, "/error.log"), level: "debug"})
     ]
 });
 
