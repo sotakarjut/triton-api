60833d72ceeb6b43c92d96b9af9b9e2c772dfda8
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-08-26 20:35:24 +0300

    Added persistent logging to application using Winston and a Docker volume. Renamed the database volume.

--- app/src/util/logger.ts
+++ app/src/util/logger.ts
@@ -1,10 +1,17 @@
+import fs from "fs";
+import path from "path";
 import winston from "winston";
+
 import { ENVIRONMENT } from "./secrets";
+const logDir = "log";
+if ( !fs.existsSync( logDir ) ) {
+    fs.mkdirSync( logDir );
+}
 
 const logger = winston.createLogger({
     transports: [
         new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
-        // new (winston.transports.File)({ filename: "debug.log", level: "debug"})
+        new (winston.transports.File)({ filename: path.join(logDir, "/debug.log"), level: "debug"})
     ]
 });
 
