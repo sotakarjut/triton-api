23e4fec15b2473707b90b288902a4d687513060e
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-09-01 00:41:20 +0300

    Fixed typo in Role model.

--- app/src/models/Role.ts
+++ app/src/models/Role.ts
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 export type RoleModel = mongoose.Document & {
   _id: number,
   canHack: boolean,
-  canImpresonate: boolean,
+  canImpersonate: boolean,
   hackerLevel: number,
   name: string
 };
 
 const roleSchema = new mongoose.Schema({
    canHack: Boolean,
-   canImpresonate: Boolean,
+   canImpersonate: Boolean,
    hackerLevel: Number,
    name: { type: String, unique: true }
 }, );
