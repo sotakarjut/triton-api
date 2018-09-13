cd383b8f5e4c83c024ad7eb8e6228467c921175d
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-09-13 23:06:15 +0300

    Added api route for getting all news.(not connected yet)

--- app/src/controllers/news.ts
+++ app/src/controllers/news.ts
@@ -6,4 +6,25 @@ import { default as User, UserModel } from "../models/User";
 
 import { DatabaseError } from "../util/error";
 
+import { getNews } from "../services/news";
+
 const request = require("express-validator");
+
+/**
+ * @api {get} news Get all news
+ * @apiGroup News
+ * @apiDescription
+ * Returns all news articles submitted to the system.
+ *
+ * @apiError (500) DatabaseError The database search failed.
+ * @apiSuccess (200) {Object[]} news An array containing all news.
+ */
+export let getAllNews = (req: Request, res: Response) => {
+
+  getNews().then( (messages: any) => {
+     return res.status(200).send(messages);
+  }).catch( (err: DatabaseError) => {
+    return res.status(err.statusCode).send(err.message);
+  });
+
+};
--- app/src/services/news.ts
+++ app/src/services/news.ts
@@ -7,3 +7,20 @@ import { default as User, UserModel } from "../models/User";
 import { APIError, DatabaseError } from "../util/error";
 import logger from "../util/logger";
 
+export let getNews = () => {
+   return new Promise ((resolve, reject) => {
+    News.find()
+    .populate("author", "name")
+    .exec((err: mongoose.Error, news: NewsModel[]) => {
+      if (err) {
+        logger.error(err);
+        return reject( new DatabaseError(404, "Error: Could not find news"));
+      }
+      const newsMap: any = {};
+      news.forEach((piece: NewsModel ) => {
+        newsMap[piece._id] = piece;
+      });
+      return resolve(newsMap);
+    });
+  });
+};
