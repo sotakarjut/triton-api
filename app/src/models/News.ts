c87fdeb882573666c47639ccd2c759ed4da1f72c
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2018-09-13 22:24:58 +0300

    Added new model News.

---
+++ app/src/models/News.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { UserModel } from "./User";
+export type NewsModel = mongoose.Document & {
+  _id: number,
+  body: string,
+  author: UserModel,
+  title: string
+};
+
+const newsSchema = new mongoose.Schema({
+   body: String,
+   sender: { type: mongoose.Schema.Types.ObjectId, ref: "User",  },
+   title: String,
+}, { timestamps: true });
+
+const newsModel = mongoose.model("News", newsSchema);
+export default newsModel;
